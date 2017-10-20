cf1aac75dbeda599c6660373fe2ce2b022086a7d
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-20 09:34:20 -0700

    Creates and wires up the menu to clear strokes

--- app/src/main/java/com/chrisgaona/golfscorecard/MainActivity.java
+++ app/src/main/java/com/chrisgaona/golfscorecard/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends ListActivity {
     private static final String PREFS_FILE = "com.chrisgaona.golfscorecard.preferences";
@@ -43,5 +45,40 @@ public class MainActivity extends ListActivity {
 
         // apply saves the changes
         mEditor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle action bar item clicks here.
+        // the action bar will automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        // no inspection Simplifiable if statement
+        if (id == R.id.action_clear_strokes) {
+            // removes all shared preference values
+            mEditor.clear();
+            // save changes
+            mEditor.apply();
+
+            // for each loop example instead of simple for loop
+            for (Hole hole: mHoles) {
+                hole.setStrokeCount(0);
+            }
+
+            // tells listAdapter to update due to data set changes
+            mListAdapter.notifyDataSetChanged();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

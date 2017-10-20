e2218172e86814662b603771b0ab032a71b375ea
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-20 09:17:21 -0700

    Implements shared preferences to score stroke counts

--- app/src/main/java/com/chrisgaona/golfscorecard/MainActivity.java
+++ app/src/main/java/com/chrisgaona/golfscorecard/MainActivity.java
@@ -1,9 +1,15 @@
 package com.chrisgaona.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private static final String PREFS_FILE = "com.chrisgaona.golfscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "KEY_STROKECOUNT";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
 
@@ -12,13 +18,30 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
         // initialize holes
         int strokes = 0;
         for (int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
             mHoles[i] = new Hole("Hole " + (i + 1) + " :", strokes);
         }
 
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // save the scores on pause lifecycle hook
+        for (int i = 0; i < mHoles.length; i++) {
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getStrokeCount());
+        }
+
+        // apply saves the changes
+        mEditor.apply();
     }
 }
